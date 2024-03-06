0a17350a8d4cfbec3990b213bb2e13e87ee30db3
CXRunfree <2654559534@qq.com>
2024-03-06 16:11:28 +0800

    playwright重构版

--- Autovisor.py
+++ Autovisor.py
@@ -1,39 +1,95 @@
+# encoding=utf-8
 import traceback
 import json
 import time
 from json import JSONDecodeError
+from playwright.sync_api import sync_playwright
+from playwright._impl._errors import TargetClosedError, TimeoutError
 
-from selenium import webdriver
-from selenium.common import NoSuchWindowException, JavascriptException, TimeoutException
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# constants
+login_url = "https://passport.zhihuishu.com/login"
+# Xpath
+option1 = '//*[@id="playTopic-dialog"]/div/div[2]/div/div[1]/div/div/div[2]/ul/li[1]/div[2]'
+option2 = '//*[@id="playTopic-dialog"]/div/div[2]/div/div[1]/div/div/div[2]/ul/li[2]/div[2]'
+# javascript
+# 登录
+login_js = '''document.getElementsByClassName("wall-sub-btn")[0].click();'''
+block_js = '''return document.getElementsByClassName("yidun_jigsaw")[0].src'''
+bg_js = '''return document.getElementsByClassName("yidun_bg-img")[0].src'''
+# 弹窗
+pop_js = '''document.getElementsByClassName("iconfont iconguanbi")[0].click();'''
+# pop2_js = '''document.evaluate('//*[@id="app"]/div/div[1]/div[1]/span/a',document).iterateNext().click();'''
+# 其他
+night_js = '''document.getElementsByClassName("Patternbtn-div")[0].click()'''
 
 
-def add_opts(option):
-    option.add_argument('--ignore-certificate-errors')
-    option.add_argument("disable-extensions")
-    option.add_experimental_option("excludeSwitches", ["enable-logging"])
+def auto_login(_user, _pwd):
+    if not user or not pwd:
+        raise UserWarning
+    page.goto(login_url)
+    page.locator('#lUsername').fill(_user)
+    page.locator('#lPassword').fill(_pwd)
+    page.wait_for_timeout(500)
+    page.evaluate(login_js)
 
 
-def driver_init(driver_type):
-    if driver_type == "Edge":
-        options = webdriver.EdgeOptions()
-        add_opts(options)
-        _driver = webdriver.Edge(options)
-    elif driver_type == "Chrome":
-        options = webdriver.ChromeOptions()
-        add_opts(options)
-        _driver = webdriver.Chrome(options)
+def init_page():
+    # 启动自带浏览器
+    if driver == "Chrome":
+        print("正在启动Chrome浏览器...")
+        browser = p.chromium.launch(channel="chrome", headless=False)
     else:
-        _driver = None
-    return _driver
+        print("正在启动Edge浏览器...")
+        browser = p.chromium.launch(channel="msedge", headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    # 设置程序超时时限
+    page.set_default_timeout(300 * 1000)
+    # 设置浏览器视口大小
+    viewsize = page.evaluate('''() => {
+                       return {width: window.screen.availWidth,height: window.screen.availHeight};}''')
+    viewsize["height"] -= 50
+    page.set_viewport_size(viewsize)
+    return page
+
+
+def optimize_page():
+    # 关闭学习须知
+    page.evaluate(pop_js)
+    # 根据当前时间切换夜间模式
+    hour = time.localtime().tm_hour
+    if hour >= 18 or hour < 7:
+        page.wait_for_selector(".Patternbtn-div")
+        page.evaluate(night_js)
+    try:
+        # 关闭上方横幅
+        page.wait_for_selector(".exploreTip", timeout=500)
+        page.query_selector('a:has-text("不再提示")').click()
+    finally:
+        return
+
+
+def get_lesson_name():
+    title_ele = page.wait_for_selector("#lessonOrder")
+    page.wait_for_timeout(500)
+    title_ = title_ele.get_attribute("title")
+    return title_
+
+
+def move_mouse(elem):
+    elem.hover()
+    pos = elem.bounding_box()
+    # 计算移动的目标位置
+    target_x = pos['x'] + 30
+    target_y = pos['y'] + 30
+    page.mouse.move(target_x, target_y)
 
 
 def get_progress():
     curt = "进度获取中..."
-    text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "passTime")))
+    canvas = page.wait_for_selector(".videoArea")
+    move_mouse(canvas)
+    text = page.query_selector(".passTime")
     try:
         # 最多保留2位小数;不足则输出原形
         s = text.get_attribute('style').split(": ")[1][:-1]
@@ -47,177 +103,143 @@ def get_progress():
 
 
 def check_play():
-    canvas = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "videoArea")))
-    ActionChains(driver).move_to_element(canvas).click().perform()
+    canvas = page.wait_for_selector(".videoArea")
+    move_mouse(canvas)
+    canvas.click()
 
 
 def shut_volume():
-    driver.execute_script(show_controlsBar)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "volumeBox"))).click()
+    canvas = page.wait_for_selector(".videoArea")
+    move_mouse(canvas)
+    page.wait_for_selector(".volumeBox").click()
 
 
 def play_next():
-    driver.execute_script(show_controlsBar)
-    next_but = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "nextBtn")))
+    canvas = page.wait_for_selector(".videoArea")
+    move_mouse(canvas)
+    next_but = page.wait_for_selector("#nextBtn")
+    page.wait_for_timeout(200)
     next_but.click()
 
 
-def get_lesson_name():
-    title_ = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "lessonOrder"))).get_attribute("title")
-    return title_
-
-
 def skip_question():
-    WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, option2))).click()
-    driver.find_element(By.XPATH, option1).click()
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, x).click()
+    try:
+        page.wait_for_selector(".topic-item", timeout=2000)
+        choices = page.query_selector_all(".topic-item")
+        choices[0].click()
+        choices[1].click()
+        page.wait_for_timeout(500)
+        page.query_selector_all(".btn")[3].click()
+    except TimeoutError:
+        return
 
 
-# 初始化配置>>>>>>>>>>>>>>>>>>>>
-print("载入数据...")
-# 读取用户信息
-try:
-    with open('./account.json', 'r', encoding='utf-8') as f:
-        account = json.load(f)
-except JSONDecodeError:
-    print("account文件内容有误!")
-    exit(-1)
-
-# 启动driver
-driver = driver_init(account["Driver"].strip())
-if not driver:
-    print("暂不支持该浏览器,请检查配置!")
-    exit(-1)
+def detect_auth():
+    try:
+        page.wait_for_selector(".yidun_modal__title", timeout=1000)
+        hasAuth = True
+    except TimeoutError:
+        hasAuth = False
+    return hasAuth
 
 
-# 隐藏浏览器指纹
-with open("Res/stealth.min.js", "r") as f:
-    js = f.read()
-driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {"source": js})
-
-
-# constants
-login_url = "https://passport.zhihuishu.com/login"
-lesson_finish = True
-user = account['User'].strip()
-pwd = account['Password'].strip()
-
-# ElementXpath
-drag_bar = "/html/body/div[31]/div[2]/div/div/div[2]/div/div[2]/div[2]"
-option1 = '//*[@id="playTopic-dialog"]/div/div[2]/div/div[1]/div/div/div[2]/ul/li[1]/div[2]'
-option2 = '//*[@id="playTopic-dialog"]/div/div[2]/div/div[1]/div/div/div[2]/ul/li[2]/div[2]'
-x = '/html/body/div[1]/div/div[2]/div[1]/div[2]/div[1]/div/div[3]/span/div'
-
-# javascript
-# 登录
-user_js = f'''document.getElementById("lUsername").value="{user}";'''
-pwd_js = f'''document.getElementById("lPassword").value="{pwd}";'''
-login_js = '''document.getElementsByClassName("wall-sub-btn")[0].click();'''
-block_js = '''return document.getElementsByClassName("yidun_jigsaw")[0].src'''
-bg_js = '''return document.getElementsByClassName("yidun_bg-img")[0].src'''
-# 弹窗
-pop_late = '''document.getElementsByClassName("el-dialog__close el-icon el-icon-close")[2].click()'''
-pop_js = '''document.getElementsByClassName("iconfont iconguanbi")[0].click();'''
-pop2_js = '''document.evaluate('//*[@id="app"]/div/div[1]/div[1]/span/a',document).iterateNext().click();'''
-# 其他
-night_js = '''document.getElementsByClassName("Patternbtn-div")[0].click()'''
-show_controlsBar = '''document.getElementsByClassName("controlsBar")[0].setAttribute("style","z-index: 2; overflow: inherit;")'''
-
-try:
-    print("正在登录...")
-    # 打开登录页面,自动填充账户密码
-    if not user or not pwd:
-        raise UserWarning
-    driver.get(login_url)
-    driver.execute_script(user_js)
-    driver.execute_script(pwd_js)
-    time.sleep(0.5)
-    driver.execute_script(login_js)
+def main_func():
+    # 进行登录
+    print("等待登录完成...")
+    auto_login(user, pwd)
     # 等待完成滑块验证,已设置5min等待时间
-    WebDriverWait(driver, 300).until(
-        EC.invisibility_of_element((By.CLASS_NAME, "wall-main")))
+    page.wait_for_selector(".wall-main", state="hidden")
     # 遍历所有课程,加载网页
-    for course_url in account["Url"]:
-        if course_url.strip()[:4] != "http":
+    for course_url in urls:
+        if "CourseId" not in course_url.strip():
             continue
         print("加载播放页...")
-        driver.get(course_url)
-        # 等待开屏弹窗出现
-        WebDriverWait(driver, 300).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "studytime-div")))
-        time.sleep(0.5)
-        try:
-            # 关闭课程结束提示
-            driver.execute_script(pop_late)
-            # 关闭上方横幅
-            driver.execute_script(pop2_js)
-        except JavascriptException:
-            pass  # 不是每次都有此类弹窗
-        # 关闭学习须知
-        driver.execute_script(pop_js)
-        # 根据当前时间切换夜间模式
-        hour = time.localtime().tm_hour
-        if hour >= 18 or hour < 7:
-            driver.execute_script(night_js)
-
-        # 遍历课程小节
+        page.goto(course_url)
+        page.wait_for_selector(".studytime-div")
+        # 关闭弹窗,优化页面体验
+        optimize_page()
+        # 获取当前课程名
+        course_title = page.wait_for_selector(".source-name").text_content()
+        print(f"当前课程:<<{course_title}>>")
+        start_time = time.time()  # 记录开始学习时间
         while True:
-            # 获取课程名
+            # 获取课程小节名
             title = get_lesson_name()
             print("正在学习:%s" % title)
-            # 开始播放
-            curtime = "0"
-            check_play()
-            # 设置为静音
-            shut_volume()
+            # 根据进度条判断播放状态
+            curtime = get_progress()
+            check_play()  # 开始播放
+            shut_volume()  # 设置为静音
+            page.set_default_timeout(2000)
             while curtime != "100%":
                 try:
-                    # 跳过中途弹题(只支持选择题)
-                    skip_question()
-                except TimeoutException:
-                    pass  # 并非每时都要跳过答题
-                curtime2 = get_progress()
-                if curtime2 != curtime:  # 进度条若相同则说明视频已暂停
-                    curtime = curtime2
-                    print('已完成:%s' % curtime)
-                else:
-                    check_play()
-                    print("检测到视频暂停,已自动继续..")
-                    title = get_lesson_name()
-                    print("正在学习:%s" % title)
-            # 进度100%时开始下一集
+                    skip_question()  # 跳过中途弹题(只支持选择题)
+                    curtime2 = get_progress()
+                    if curtime2 != curtime:  # 进度条若相同则说明视频已暂停
+                        curtime = curtime2
+                        print('完成进度:%s' % curtime)
+                    else:
+                        check_play()
+                        print("检测到视频暂停,已自动继续..")
+                        title = get_lesson_name()
+                        print("正在学习:%s" % title)
+                except TimeoutError:
+                    input("请完成安全验证,按Enter继续:")
             try:
-                play_next()
+                page.set_default_timeout(300 * 1000)
+                play_next()  # 进度100%时开始下一集
+                time_period = (time.time() - start_time) / 60
+                if time_period >= 1:  # 每完成一节提示一次时间
+                    print("本次课程已学习:%.1f min" % time_period)
             except Exception:  # 捕获有时点击失败的情况
                 continue
             # 延时获取新数据
-            time.sleep(1.5)
+            page.wait_for_timeout(1500)
             title2 = get_lesson_name()
             # 点完下一集后若标题不改变,判定为课程结束
             if title2 != title:
                 print(f"\"{title}\" Done !")
             else:
                 print("已学完本课程全部内容!")
-                print(">>" * 10)
+                print("==" * 10)
                 break
-        time.sleep(1)
-# 特殊异常捕获
-except Exception as e:
-    print(f"Error:{type(e)}")
-    if type(e) == UserWarning:
-        print("哎呀,是不是忘填账号密码了~")
-    elif type(e) == NoSuchWindowException:
-        print("糟糕!网页关闭了~")
-    else:
-        log = traceback.format_exc()
-        print("错误日志已保存至:log.txt")
-        with open("./log.txt", "w", encoding="utf-8") as doc:
-            doc.write(log)
-    print("系统异常,要不重启一下?")
-    lesson_finish = False
-# 程序出口
-driver.quit()
-if lesson_finish:
-    print("所有课程学习完毕!")
-input()
+            page.wait_for_timeout(1000)
+
+
+if __name__ == "__main__":
+    print("===== Runtime Log =====")
+    try:
+        print("载入数据...")
+        with open("account.json", "r", encoding="utf-8") as f:
+            account = json.loads(f.read())
+        user = account["User"].strip()
+        pwd = account["Password"].strip()
+        driver = account["Driver"].strip()
+        urls = account["Url"]
+        if not isinstance(urls, list):
+            print('[Error]"Url"项格式错误!')
+            raise KeyError
+        with sync_playwright() as p:
+            page = init_page()
+            main_func()
+            print("所有课程学习完毕!")
+    except Exception as e:
+        if isinstance(e, JSONDecodeError):
+            print("[Error]account文件内容有误!")
+        elif isinstance(e, KeyError):
+            print("[Error]可能是account文件的配置出错!")
+        elif isinstance(e, UserWarning):
+            print("[Error]是不是忘记填账号密码了?")
+        elif isinstance(e, FileNotFoundError):
+            print("[Error]程序缺失依赖文件,请重新安装程序!")
+        elif isinstance(e, TargetClosedError):
+            print("[Error]糟糕,网页关闭了!")
+        elif isinstance(e, TimeoutError):
+            print("[Error]程序长时间无响应,自动退出...")
+        else:
+            print(f"[Error]{e}")
+            with open("log.txt", "w", encoding="utf-8") as doc:
+                doc.write(traceback.format_exc())
+            print("错误日志已保存至:log.txt")
+            print("系统出错,要不重启一下?")
+    input()
