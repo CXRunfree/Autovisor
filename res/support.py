1498865019cfe358a6471113f4c7a9055cccef73
CXRunfree <2654559534@qq.com>
2024-04-09 12:06:57 +0800

    修复路径问题

--- res/support.py
+++ res/support.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 def show_donate():
     try:
-        img = Image.open("QRcode.jpg")
+        img = Image.open("res/QRcode.jpg")
         print("感觉还不错? 来请作者喝杯coffee~")
         img.show()
         print("不希望显示赞赏码? 删除res文件夹的QRcode文件就好啦~")
